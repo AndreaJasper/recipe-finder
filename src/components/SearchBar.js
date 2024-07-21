655c1efc8f5303a230a6cb482a34579bdadae7d8
jasper_a <andrea.nicole.jasper@gmail.com>
2024-07-20 18:46:17 -0600

    adds imports to SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,12 @@
-export default function SearchBar() {
+import React, {useState} from "react";
+import { fetchRecies } from "../api/edamamApi";
+
+function SearchBar() {
   return (
     <>
       <h1>Hello there</h1>
     </>
   );
 }
+
+export default SearchBar;
